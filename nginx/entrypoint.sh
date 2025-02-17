3365af70d1b2ccc0fb8fa9d887372a8ad3600d1a
Gervasio Marchand <github@gervas.io>
2025-02-17 11:19:26 -0300

    even fewer logs

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-inotifywait -m -e close_write /etc/nginx/conf.d/ | while read path action file; do
+inotifywait -m -q -e close_write /etc/nginx/conf.d/ | while read path action file; do
   echo "[inotify] Detected a change in $path$file — reloading Nginx..."
-  nginx -s reload
+  nginx -s reload 2>/dev/null
 done &
 
 exec "$@"
